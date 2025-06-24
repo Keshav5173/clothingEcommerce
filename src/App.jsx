99db67d8fce733d4ebc756ccc9a4e7f30039d37c
Keshav <bhardwajkeshav5173@gmail.com>
2025-06-24 07:29:29 +0530

    Ready for production

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import './App.css'
 import { Routes, Route, Navigate } from "react-router-dom";
 
 // import pages and components
-import HomePage from './pages/homePage';
-import ProductDetails from './pages/prouctDets';
+import HomePage from './pages/homePage.jsx';
+import ProductDetails from './pages/productDetails.jsx';
 
 
 
@@ -12,10 +12,10 @@ function App() {
   
   return (
     // <ProductCardComponet />
-    <Routes>
-      <Route path="/" element={<HomePage />}/>
-      <Route path="/product/:id" element={<ProductDetails />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+      </Routes>
   )
 }
 
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -120,8 +120,8 @@ function HomePage() {
       </div>
 
       {loading ? (
-        <div class="flex items-center justify-center h-screen bg-white">
-          <div class="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+        <div className="flex items-center justify-center h-screen bg-white">
+          <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
         </div>
 
       ) : error ? (
